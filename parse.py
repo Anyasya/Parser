a4dc1d187a618932ccd7a0c58446ec3e4c2464b1
anyasya <anyasya1999@gmail.com>
2021-05-03 19:31:26 +0300

    Third part

--- parse.py
+++ parse.py
@@ -2,15 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-URL = 'https://belgorod.ml-respect.ru/car/kia/'
+URL = 'https://belgorod.ml-respect.ru/car/audi'
+
 HEADERS = {
     'user-agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
     'accept': '*/*'
     }
 
+HOST = 'https://belgorod.ml-respect.ru'
 def get_html(url, params=None):  # parse all pages
     r = requests.get(url, headers=HEADERS, params=params)
     return r
+
+def get_pages_count(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    pagination = soup.find('ul', class_='page_list')
+    if pagination:
+        return pagination.text[-2]
+    else:
+        return 1
+
+
 
 def get_content(html):
     soup = BeautifulSoup(html,'html.parser')
@@ -19,8 +31,7 @@ def get_content(html):
     for item in items:
         cars.append({
             'title': item.find('div', class_='box-title').get_text(strip=True),
-            #'link': item.find('a', class_='box-title', href = re.compile(r'[/]([a-z]|[A-Z])\w+')).attrs['href'],
-            'link': item.find('a',class_='box-images transparent',href=True).get("href"),
+            'link': HOST + item.find('a',class_='box-images transparent',href=True).get("href"),   # find.find_next
             'price': item.find('div', class_='new-price').get_text(),
         })
     print(cars)
@@ -30,6 +41,8 @@ def parse():
     html = get_html(URL)
     if html.status_code == 200:
         print('Подключение успешно')
+        pages_count = get_pages_count(html.text)
+        print(pages_count)
         print(get_content(html.text))
     else:
         print('Ошибка подключения')
