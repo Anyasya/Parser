e4eb222b6f0c279604aab773768c8cf7032231c2
anyasya <anyasya1999@gmail.com>
2021-05-03 21:14:53 +0300

    Final part

--- parse.py
+++ parse.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-import re
+import csv
 
-URL = 'https://belgorod.ml-respect.ru/car/audi'
+URL = 'https://belgorod.ml-respect.ru/car/'
 
 HEADERS = {
     'user-agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
@@ -10,6 +10,8 @@ HEADERS = {
     }
 
 HOST = 'https://belgorod.ml-respect.ru'
+
+FILE = 'cars.csv'
 def get_html(url, params=None):  # parse all pages
     r = requests.get(url, headers=HEADERS, params=params)
     return r
@@ -22,6 +24,12 @@ def get_pages_count(html):
     else:
         return 1
 
+def save_file(items,path):
+     with open (path,'w',newline='') as file:
+         writer = csv.writer(file,delimiter = ';')
+         writer.writerow(['Марка','Цена','Ссылка'])
+         for item in items:
+             writer.writerow([item['title'], item['price'], item['link']])
 
 
 def get_content(html):
@@ -32,18 +40,24 @@ def get_content(html):
         cars.append({
             'title': item.find('div', class_='box-title').get_text(strip=True),
             'link': HOST + item.find('a',class_='box-images transparent',href=True).get("href"),   # find.find_next
-            'price': item.find('div', class_='new-price').get_text(),
+            'price': item.find('div', class_='new-price').get_text()
         })
-    print(cars)
+    return cars
 
 
 def parse():
     html = get_html(URL)
     if html.status_code == 200:
-        print('Подключение успешно')
-        pages_count = get_pages_count(html.text)
-        print(pages_count)
-        print(get_content(html.text))
+        cars = []
+        page_count = int(get_pages_count(html.text))
+        for page in range(1,page_count + 1):
+            print('Парсинг',page,'страницы из',page_count,'страниц...')
+            html = get_html(URL + f'?PAGEN_1={page}')
+            cars.extend(get_content(html.text))
+            save_file(cars, FILE)
+        print('Сохранено', len(cars), 'запись(-ей)')
+
+
     else:
         print('Ошибка подключения')
 
